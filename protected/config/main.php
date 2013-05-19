b45cbf5ef3017281c6d63dd76f02d784d0bdeb71
Bryan Wyatt <brwyatt@gmail.com>
2013-05-18 20:04:57 -0500

    updating names and titles

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Jungle Cat Software',
+	'name'=>'DuneCat\'s Snacks',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
--- protected/config/params.php
+++ protected/config/params.php
@@ -3,11 +3,11 @@
 // this contains the application parameters that can be maintained via GUI
 return array(
 	// this is displayed in the header section
-	'title'=>'Jungle Cat Software',
+	'title'=>'DuneCat\'s Snacks',
 	// this is used in error pages
-	'adminEmail'=>'webmaster@junglecatsoftware.com',
+	'adminEmail'=>'webmaster@brwyatt.net',
 	// the copyright information displayed in the footer section
-	'copyrightInfo'=>'<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/us/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">junglecatsoftware.com</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://junglecatsoftware.com/" property="cc:attributionName" rel="cc:attributionURL">Jungle Cat Software</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 United States License</a>.',
+	'copyrightInfo'=>'<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/us/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">DuneCat\'s Snacks</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://brwyatt.net/" property="cc:attributionName" rel="cc:attributionURL">Bryan Wyatt</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/us/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 United States License</a>.',
 );
 
 ?>
