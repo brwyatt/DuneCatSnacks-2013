96e6c7fd3c5ae66aa4d1f3d9ec47f62e7670864f
Bryan Wyatt <brwyatt@gmail.com>
2013-07-29 22:09:41 -0500

    Adding Image for Items

--- protected/views/item/_view.php
+++ protected/views/item/_view.php
@@ -5,29 +5,24 @@
 
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('iditem')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->iditem), array('view', 'id'=>$data->iditem)); ?>
-	<br />
+        <b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+        <?php echo CHtml::encode($data->name); ?>
+        <br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
-	<?php echo CHtml::encode($data->name); ?>
-	<br />
+        <img src="/assets/itemImages/<?php echo CHtml::encode($data->image); ?>" />
+        <br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
-	<?php echo CHtml::encode($data->description); ?>
-	<br />
+        <b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
+        <?php echo CHtml::encode($data->description); ?>
+        <br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('image')); ?>:</b>
-	<?php echo CHtml::encode($data->image); ?>
-	<br />
+        <b><?php echo CHtml::encode($data->getAttributeLabel('price')); ?>:</b>
+        <?php echo CHtml::encode($data->price); ?>
+        <br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('price')); ?>:</b>
-	<?php echo CHtml::encode($data->price); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('quantity')); ?>:</b>
-	<?php echo CHtml::encode($data->quantity); ?>
-	<br />
+        <b><?php echo CHtml::encode($data->getAttributeLabel('quantity')); ?>:</b>
+        <?php echo CHtml::encode($data->quantity); ?>
+        <br />
 
 
 </div>
