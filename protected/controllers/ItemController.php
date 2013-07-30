ba076435128a216b7e99cefef156b053a967c1a7
Bryan Wyatt <brwyatt@gmail.com>
2013-07-29 22:17:17 -0500

    Fixing permissions error

--- protected/controllers/ItemController.php
+++ protected/controllers/ItemController.php
@@ -37,7 +37,7 @@ class ItemController extends Controller
 			),*/
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','create','update','delete'),
-				'expression'=>'Yii::app()->user->role=="admin"'
+				'expression'=>'(isset(Yii::app()->user->role) && (Yii::app()->user->role=="admin"))'
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
